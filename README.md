699af63eb3f20b8a71ee845db98ebd5e1d559603
Gengsu07 <sugengdatascience@gmail.com>
2024-04-04 12:00:11 +0700

    add image , on progres readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## End To End Data Pipeline Games Released 2024
+
 ### Mage Deploy to GCP with Terraform
 
 https://docs.mage.ai/production/deploying-to-cloud/using-terraform
@@ -10,3 +12,11 @@ https://docs.mage.ai/production/deploying-to-cloud/using-terraform
 4. terraform init
 5. terraform plan
    6terraform apply
+
+---
+
+Mage AI hosted on Google Cloud: https://gengsudezoomcamp1-lhyfb2n2qq-wl.a.run.app
+![1712206355848](image/README/1712206355848.png)
+
+Metabase hosted on Google Cloud: https://gengsudezoomcamp1-metabase-lhyfb2n2qq-wl.a.run.app
+![1712206254213](image/README/1712206254213.png) http://gengsudezoomcamp1-metabase-lhyfb2n2qq-wl.a.run.app/public/dashboard/2bf30e19-d241-4a67-9330-bab53c9706e4
--- analytics.sql
+++ analytics.sql
@@ -1,3 +1,54 @@
+--- BigQuery
+with df as (SELECT 
+EXTRACT(MONTH FROM fr.released)AS `Month`,
+FORMAT_DATE('%B', fr.released)AS `Month Name`, 
+count(fr._dlt_id) AS `Count` 
+FROM rawg_fctTable.fctRawg fr  
+GROUP BY EXTRACT(MONTH FROM fr.released),
+FORMAT_DATE('%B', fr.released))
+select * from df
+order by df.`Month` asc
+
+
+SELECT 
+'PC' AS `Platform`,
+count(CASE WHEN LOWER(fr.platform_name) LIKE '%pc%' THEN _dlt_id END) AS `Count`
+FROM  rawg_fctTable.fctRawg fr 
+UNION ALL
+SELECT 
+'Linux',
+count(CASE WHEN LOWER(fr.platform_name) LIKE '%linux%' THEN _dlt_id END) AS `Count`
+FROM  rawg_fctTable.fctRawg fr 
+UNION ALL
+SELECT 
+'macOS',
+count(CASE WHEN LOWER(fr.platform_name) LIKE '%macos%' THEN _dlt_id END) AS `macOS`
+FROM  rawg_fctTable.fctRawg fr 
+UNION ALL
+SELECT 
+'PS5',
+count(CASE WHEN LOWER(fr.platform_name) LIKE '%playStation 5%' THEN _dlt_id END) AS `PS5`
+FROM  rawg_fctTable.fctRawg fr 
+UNION ALL
+SELECT 
+'Nintendo',
+count(CASE WHEN LOWER(fr.platform_name) LIKE '%nintendo switch%' THEN _dlt_id END) AS `Nintendo`
+FROM  rawg_fctTable.fctRawg fr 
+UNION ALL
+SELECT 
+'XBox',
+count(CASE WHEN LOWER(fr.platform_name) LIKE '%xbox%' THEN _dlt_id END) AS `XBOX`
+FROM  rawg_fctTable.fctRawg fr 
+UNION ALL
+SELECT 
+'Android',
+count(CASE WHEN LOWER(fr.platform_name) LIKE '%android%' THEN _dlt_id END) AS `Android`
+FROM  rawg_fctTable.fctRawg fr 
+
+
+
+
+--- Postgres
 SELECT 
 'PC' AS "Platform",
 count(CASE WHEN fr.platform_name ILIKE '%PC%' THEN _dlt_id END) AS "Count"
